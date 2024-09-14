9481915d0c28975885c6d7c83c690f193dba3993
Mooooun <petercomyn@gmail.com>
2024-09-14 16:31:35 +0200

    commit4

--- Assets/Script/IA/EnemyAI.cs
+++ Assets/Script/IA/EnemyAI.cs
@@ -129,9 +129,12 @@ public class EnemyAI : MonoBehaviour
             agent.enabled = false;
             enabled = false;
 
-            // Stop and disable the audio source
+            // Stop and destroy the audio source
             audioSource.Stop();
-            audioSource.enabled = false;
+            Destroy(audioSource);
+
+            // Destroy the enemy completely after 3 seconds (to allow death animation to play)
+            Destroy(gameObject, 3f);
         }
         else
         {
